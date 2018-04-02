84dbdb0dce228741eb6b10737c13695b8bea1164
piyingjie <540330792@qq.com>
2018-04-02 10:28:31 +0800

    开始的图片显示

--- app/src/main/java/com/lovebus/activity/StartPictureActivity.java
+++ app/src/main/java/com/lovebus/activity/StartPictureActivity.java
@@ -1,7 +1,11 @@
 package com.lovebus.activity;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 import bus.android.com.lovebus.R;
 
@@ -11,5 +15,15 @@ public class StartPictureActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start_picture);
+        Timer timer = new Timer();
+        TimerTask timerTask = new TimerTask() {
+            @Override
+            public void run() {
+                startActivity(new Intent(StartPictureActivity.this, Main_Activity.class));
+                finish();//关闭启动界面
+            }
+        };
+        timer.schedule(timerTask, 2000);
     }
+
 }
