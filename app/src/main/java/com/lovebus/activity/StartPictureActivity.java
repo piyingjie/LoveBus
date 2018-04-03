5ebda6e75be045863d6cf24d0ec71b9fe16d3f6f
piyingjie <540330792@qq.com>
2018-04-03 15:04:01 +0800

    首次登陆不同界面

--- app/src/main/java/com/lovebus/activity/StartPictureActivity.java
+++ app/src/main/java/com/lovebus/activity/StartPictureActivity.java
@@ -1,6 +1,7 @@
 package com.lovebus.activity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -10,17 +11,35 @@ import java.util.TimerTask;
 import bus.android.com.lovebus.R;
 
 public class StartPictureActivity extends AppCompatActivity {
-
+    private SharedPreferences sharedPreferences;
+    private boolean first_start;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start_picture);
+        sharedPreferences = getSharedPreferences("setting", MODE_PRIVATE);
+        if(sharedPreferences.getBoolean("first_start",true)==true)
+        {
+            SharedPreferences.Editor editor = sharedPreferences.edit();
+            editor.putBoolean("first_start", false);
+            editor.commit();
+            first_start=true;
+        }
+        else {
+            first_start=false;
+        }
         Timer timer = new Timer();
         TimerTask timerTask = new TimerTask() {
             @Override
             public void run() {
-                startActivity(new Intent(StartPictureActivity.this, Main_Activity.class));
-                finish();//关闭启动界面
+                if(first_start==false) {
+                    startActivity(new Intent(StartPictureActivity.this, Main_Activity.class));
+                    finish();//关闭启动界面
+                }
+                else {
+                    startActivity(new Intent(StartPictureActivity.this, FirstLoginActivity.class));
+                    finish();//关闭启动界面
+                }
             }
         };
         timer.schedule(timerTask, 2000);
