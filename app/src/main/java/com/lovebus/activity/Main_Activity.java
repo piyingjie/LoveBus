fd0b04119bbe1bee1274aa627abfc2dd41921501
piyingjie <540330792@qq.com>
2018-04-04 11:21:00 +0800

    去掉了百度地图

--- app/src/main/java/com/lovebus/activity/Main_Activity.java
+++ app/src/main/java/com/lovebus/activity/Main_Activity.java
@@ -2,11 +2,9 @@ package com.lovebus.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
 import bus.android.com.lovebus.R;
 
 public class Main_Activity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
