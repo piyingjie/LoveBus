a930ee6cbdbe3595d1aeefa5b4a988d8f6a7284a
piyingjie <540330792@qq.com>
2018-04-08 16:00:11 +0800

    开启时地图缩放比例

--- app/src/main/java/com/lovebus/activity/Main_Activity.java
+++ app/src/main/java/com/lovebus/activity/Main_Activity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.amap.api.maps2d.AMap;
+import com.amap.api.maps2d.CameraUpdateFactory;
 import com.amap.api.maps2d.MapView;
 import com.amap.api.maps2d.model.MyLocationStyle;
 
@@ -24,6 +25,7 @@ public class Main_Activity extends AppCompatActivity {
         if (aMap == null) {
             aMap = mMapView.getMap();
         }
+        aMap.moveCamera(CameraUpdateFactory.zoomTo(30));
         MyLocationStyle myLocationStyle;
         myLocationStyle = new MyLocationStyle();//初始化定位蓝点样式类myLocationStyle.myLocationType(MyLocationStyle.LOCATION_TYPE_LOCATION_ROTATE);//连续定位、且将视角移动到地图中心点，定位点依照设备方向旋转，并且会跟随设备移动。（1秒1次定位）如果不设置myLocationType，默认也会执行此种模式。
         myLocationStyle.interval(2000); //设置连续定位模式下的定位间隔，只在连续定位模式下生效，单次定位模式下不会生效。单位为毫秒。
