5abb761f4574bc41c95e5114b7314247f8469dea
piyingjie <540330792@qq.com>
2018-04-09 16:13:09 +0800

    基础的搜索框v1.2.2

--- app/src/main/java/com/lovebus/view/BottomMenu.java
+++ app/src/main/java/com/lovebus/view/BottomMenu.java
@@ -1,16 +1,13 @@
 package com.lovebus.view;
-
 import android.content.Context;
 import android.util.AttributeSet;
 import android.view.LayoutInflater;
 import android.widget.RelativeLayout;
 
 import bus.android.com.lovebus.R;
-
-/*自定义搜索框的定义*/
-public class SearchLayout extends RelativeLayout{
-    public SearchLayout(Context context, AttributeSet attrs){
+public class BottomMenu extends RelativeLayout{
+    public BottomMenu(Context context, AttributeSet attrs){
         super(context, attrs);
-        LayoutInflater.from(context).inflate(R.layout.search_layout, this);
+        LayoutInflater.from(context).inflate(R.layout.bottom_menu, this);
     }
 }
