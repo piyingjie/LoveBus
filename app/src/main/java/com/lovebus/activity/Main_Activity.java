32ce1517e1498060753c6a8e4227ea3f833d376e
haoran <421679009@qq.com>
2018-04-11 10:55:42 +0800

    Floating location debug

--- app/src/main/java/com/lovebus/activity/Main_Activity.java
+++ app/src/main/java/com/lovebus/activity/Main_Activity.java
@@ -5,10 +5,12 @@ import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageButton;
 
 import com.amap.api.maps2d.AMap;
+import com.amap.api.maps2d.AMapOptions;
 import com.amap.api.maps2d.CameraUpdateFactory;
 import com.amap.api.maps2d.MapView;
 import com.amap.api.maps2d.model.MyLocationStyle;
@@ -73,12 +75,16 @@ public class Main_Activity extends AppCompatActivity implements View.OnClickList
         //开始的缩放比例
         MyLocationStyle myLocationStyle;
         myLocationStyle = new MyLocationStyle();
+
         //初始化定位蓝点样式类
         // myLocationStyle.myLocationType(MyLocationStyle.LOCATION_TYPE_LOCATION_ROTATE);
         // 连续定位、且将视角移动到地图中心点，定位点依照设备方向旋转，并且会跟随设备移动。（1秒1次定位）如果不设置myLocationType，默认也会执行此种模式。
         myLocationStyle.interval(2000);
         //设置连续定位模式下的定位间隔，只在连续定位模式下生效，单次定位模式下不会生效。单位为毫秒。
         aMap.setMyLocationStyle(myLocationStyle);
+        Log.d("TAG", "getZoomPosition() returned: " + aMap.getUiSettings().getZoomPosition());
+        aMap.getUiSettings().setZoomPosition(AMapOptions.ZOOM_POSITION_RIGHT_CENTER);
+        Log.d("TAG", "getZoomPosition() returned: " + aMap.getUiSettings().getZoomPosition());
         //设置定位蓝点的Style
         //aMap.getUiSettings().setMyLocationButtonEnabled(true);设置默认定位按钮是否显示，非必需设置。
         aMap.setMyLocationEnabled(true);
