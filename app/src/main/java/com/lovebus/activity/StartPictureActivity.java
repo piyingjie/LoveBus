84a7415fea364e4504dc4592dded08b0ee4aba1a
piyingjie <540330792@qq.com>
2018-04-11 15:49:38 +0800

    开始图片跳过功能

--- app/src/main/java/com/lovebus/activity/StartPictureActivity.java
+++ app/src/main/java/com/lovebus/activity/StartPictureActivity.java
@@ -2,22 +2,27 @@ package com.lovebus.activity;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.support.v4.view.GravityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import java.util.Timer;
 import java.util.TimerTask;
 
 import bus.android.com.lovebus.R;
 
-public class StartPictureActivity extends AppCompatActivity {
+public class StartPictureActivity extends AppCompatActivity implements View.OnClickListener{
     private SharedPreferences sharedPreferences;
     private boolean first_start;
+    Button next;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start_picture);
-
+        next=(Button)findViewById(R.id.start_next) ;
+        next.setOnClickListener(this);
         /*通过sharedpreferences保存第一次的消息*/
 
         sharedPreferences = getSharedPreferences("setting", MODE_PRIVATE);
@@ -37,16 +42,29 @@ public class StartPictureActivity extends AppCompatActivity {
         TimerTask timerTask = new TimerTask() {
             @Override
             public void run() {
-                if(first_start==false) {
-                    startActivity(new Intent(StartPictureActivity.this, Main_Activity.class));
-                    finish();//关闭启动界面
-                }
-                else {
-                    startActivity(new Intent(StartPictureActivity.this, FirstLoginActivity.class));
-                    finish();//关闭启动界面
-                }
+                jump();
             }
         };
         timer.schedule(timerTask, 2000);
+    }
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.start_next:
+                jump();
+                break;
+            default:
+        }
+        return;
+    }
+    private void jump(){
+        if(first_start==false) {
+            startActivity(new Intent(StartPictureActivity.this, Main_Activity.class));
+            finish();//关闭启动界面
+        }
+        else {
+            startActivity(new Intent(StartPictureActivity.this, FirstLoginActivity.class));
+            finish();//关闭启动界面
+        }
     }
 }
