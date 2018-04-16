bdf6773fe6ca6c953044441edf2b2915607cf6c6
piyingjie <540330792@qq.com>
2018-04-16 16:40:01 +0800

    精简界面v1.01

--- app/src/main/java/com/lovebus/activity/Main_Activity.java
+++ app/src/main/java/com/lovebus/activity/Main_Activity.java
@@ -3,13 +3,12 @@ package com.lovebus.activity;
 import android.graphics.Color;
 import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
-import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.amap.api.location.AMapLocation;
