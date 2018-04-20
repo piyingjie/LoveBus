72eff2403cf484d044ce2e4a50f623203ed5a19a
piyingjie <540330792@qq.com>
2018-04-20 09:39:07 +0800

    注册v1.0

--- app/src/main/java/com/lovebus/function/Okhttp.java
+++ app/src/main/java/com/lovebus/function/Okhttp.java
@@ -24,27 +24,6 @@ public class Okhttp {
         Request request= new Request.Builder().url(address).build();
         client.newCall(request).enqueue(callback);
     }
-    /*发送消息，参数是消息和服务器地址,键值对*/
-    public static void postOkHttpRequest(String address, RequestBody requestBody, okhttp3.Callback callback,Map<String, String> map)
-    {
-        FormBody.Builder builder = new FormBody.Builder();
-        if (map!=null)
-        {
-            //添加POST中传送过去的一些键值对信息
-            for (Map.Entry<String, String> entry:map.entrySet())
-            {
-                builder.add(entry.getKey(), entry.getValue());
-            }
-        }
-        FormBody body = builder.build();
-        OkHttpClient client= new OkHttpClient.Builder()
-                .readTimeout(READ_TIMEOUT, TimeUnit.SECONDS)//设置读取超时时间
-                .writeTimeout(WRITE_TIMEOUT,TimeUnit.SECONDS)//设置写的超时时间
-                .connectTimeout(CONNECT_TIMEOUT,TimeUnit.SECONDS)//设置连接超时时间
-                .build();
-        Request request= new Request.Builder().url(address).post(requestBody).build();
-        client.newCall(request).enqueue(callback);
-    }
     /*发送消息，参数是消息和服务器地址,json数据*/
     public static void postJson(String address, okhttp3.Callback callback, String jsonStr)
     {
